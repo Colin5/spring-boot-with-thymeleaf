ad5a214fbf375318a5142aadaf4178b3fe2419f0
Colin <colin5@163.com>
2016-09-14 15:40:52 +0800

    Initial commit

---
+++ src/main/java/com/site/main/Application.java
@@ -0,0 +1,15 @@
+package com.site.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shengquan.wu on 2016/9/14.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/com/site/main/HomeController.java
@@ -0,0 +1,19 @@
+package com.site.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by shengquan.wu on 2016/9/14.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(ModelMap map){
+        map.addAttribute("host", "http://blog.didispace.com");
+        return "index";
+    }
+
+}
